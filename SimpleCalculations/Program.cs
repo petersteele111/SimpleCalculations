217f0f01ff0e7dbd5223128291d38bac9ec9313b
Peter Steele <steelep@mail.nmc.edu>
2019-09-17 19:59:16 -0400

    Add project files.

---
+++ SimpleCalculations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculations
+{
+    class SimpleCalculations
+    {
+        static void Main(string[] args)
+        {
+            /************************************************
+            |                  Variables                    |
+            ************************************************/
+
+
+        }
+    }
+}
